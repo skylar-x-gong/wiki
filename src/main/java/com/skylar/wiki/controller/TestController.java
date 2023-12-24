20d9239e0e63cdae106cde0a585cd0d2ef156414
augustgong <1480829966@qq.com>
2023-12-24 13:28:28 -0800

    2-8 Add custom configuration settings examples

--- src/main/java/com/skylar/wiki/controller/TestController.java
+++ src/main/java/com/skylar/wiki/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.skylar.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Xiangjun Gong
- * @date 2023/12/24
+ * {@code @date} 2023/12/24
  */
 @RestController
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
 
     //@PostMapping
     //@PutMapping
@@ -16,7 +20,7 @@ public class TestController {
     //@RequestMapping(value = "user/1", method = RequestMethod.DELETE)
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }@PostMapping("/hello/post")
     public String helloPost(String name) {
         return "Hello World! Post, " + name;
