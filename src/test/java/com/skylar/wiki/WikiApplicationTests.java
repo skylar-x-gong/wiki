04469171c4749d240149dfc7b8774dc9da7a5851
augustgong <1480829966@qq.com>
2023-12-23 23:46:21 -0800

    2-2 init

---
+++ src/test/java/com/skylar/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skylar.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
