48b0bcc7253eb061ec41bda3f5a73cf07f638ffd
augustgong <1480829966@qq.com>
2023-12-25 23:53:01 -0800

    3-6 Added a common response class - CommonResp All the type of return value from back-end is CommonResp.

--- src/main/java/com/skylar/wiki/controller/EbookController.java
+++ src/main/java/com/skylar/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.skylar.wiki.controller;
 
 import com.skylar.wiki.domain.Ebook;
+import com.skylar.wiki.resp.CommonResp;
 import com.skylar.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List <Ebook>> resp = new CommonResp();
+        List <Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
