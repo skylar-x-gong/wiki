ef177f2ff1de6d8e18664fbac13ca58363a7e0f2
augustgong <1480829966@qq.com>
2023-12-28 00:38:12 -0800

    3-8 Created CopyUtil encapsulating BeanUtils

--- src/main/java/com/skylar/wiki/service/EbookService.java
+++ src/main/java/com/skylar/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.skylar.wiki.domain.EbookExample;
 import com.skylar.wiki.mapper.EbookMapper;
 import com.skylar.wiki.req.EbookReq;
 import com.skylar.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.skylar.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,13 +27,19 @@ public class EbookService {
         System.out.println(req.getName());
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+//            // EbookResp ebookResp = new EbookResp();
+//            // BeanUtils.copyProperties(ebook, ebookResp);
+//            // 对象复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//
+//            respList.add(ebookResp);
+//        }
 
-        return respList;
+        // 列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+
+        return list;
     }
 }
